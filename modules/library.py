2d5e187bf580c1b079c1caaf679977a3d10ca717
Robin de Zwart <robindezwart11@gmail.com>
2021-01-28 21:02:39 -0800

    Update library.py

--- modules/library.py
+++ modules/library.py
@@ -3,5 +3,11 @@ from .basemodule import BaseModule
 
 class Library(BaseModule):
 
+    # TODO: Four initial commands
+    #   Get url for a book
+    #   Add new book
+    #   Remove a book
+    #   List all books
+
     def __init__(self):
         pass
