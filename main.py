836257cbf8b5142daf64a04436c10149269368d6
Robin de Zwart <robindezwart11@gmail.com>
2021-01-28 21:02:43 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from configparser import ConfigParser
 
 import util
+from controller import Controller
 
 if __name__ == "__main__":
     config = ConfigParser()
     if util.check_config(config):
         print("Config file OK.")
-        pass
+
+        bot = Controller()
+        bot.connect()
+        bot.log_in()
