f06fa6661e3a54b77c50ec100728bb0c84ef961b
Robin de Zwart <robindezwart11@gmail.com>
2021-02-02 15:48:48 -0800

    Begin manager setup

--- manager.py
+++ manager.py
@@ -1,4 +1,31 @@
+import importlib
+
+import modules
+
+
 class Manager:
+    """
+
+    """
+
+    general: modules.General
+    kraken: modules.Kraken
+    lewd: modules.Lewd
+    library: modules.Library
+    rolls: modules.Rolls
 
     def __init__(self):
-        pass
+        self.load_modules()
+
+    def load_modules(self):
+        importlib.reload(modules.general)
+        importlib.reload(modules.kraken)
+        importlib.reload(modules.lewd)
+        importlib.reload(modules.library)
+        importlib.reload(modules.rolls)
+
+        self.general = modules.General()
+        self.kraken = modules.Kraken()
+        self.lewd = modules.Lewd()
+        self.library = modules.Library()
+        self.rolls = modules.Rolls()
