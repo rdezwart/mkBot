4481bc7573acc883831ee80ee1278feef8a577d7
Robin de Zwart <robindezwart11@gmail.com>
2021-02-10 15:49:26 -0800

    Add required Module functions (placeholders)

--- modules/basemodule.py
+++ modules/basemodule.py
@@ -2,4 +2,9 @@ from abc import ABC, abstractmethod
 
 
 class BaseModule(ABC):
-    pass
+
+    def read(self, controller, sender, code, send_to, line):
+        pass
+
+    def delegate(self, irc, code, line):
+        pass
