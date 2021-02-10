572d77cf2c09083f879b6f0f8fa4b50eb07f2575
Robin de Zwart <robindezwart11@gmail.com>
2021-02-10 15:50:07 -0800

    Begin moving reload code to a better location

--- modules/general.py
+++ modules/general.py
@@ -5,3 +5,6 @@ class General(BaseModule):
 
     def __init__(self):
         pass
+
+    def checkReload(self, controller, code, line, send_to):
+        controller.check_reload(code, line, send_to)
