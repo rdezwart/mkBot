0601788bb9703e3ba0f799b7366b527edb3293ea
Robin de Zwart <robindezwart11@gmail.com>
2021-02-17 17:28:30 -0800

    Implement .lib command

--- modules/library.py
+++ modules/library.py
@@ -1,4 +1,4 @@
-from configparser import ConfigParser
+from configparser import ConfigParser, DuplicateSectionError
 
 import controller
 from wrappers import cmd
@@ -7,14 +7,11 @@ from .basemodule import BaseModule
 
 class Library(BaseModule):
 
-    # TODO: Four initial commands
-    #   Get url for a book
-    #   Add new book
-    #   Remove a book
-    #   List all books
-
     def __init__(self, _config: ConfigParser):
         self.config = _config
+
+        self.library = ConfigParser()
+        self.library.read("data/library.ini")
 
     def process(self, cont: 'controller.Controller', source: str, code: str, send_to: str, line: list):
         pass
@@ -23,4 +20,83 @@ class Library(BaseModule):
 
     @cmd()
     def lib(self, cont: 'controller.Controller', source: str, code: str, send_to: str, params: list):
-        cont.chat(send_to, "Not yet implemented.")
+
+        if len(params) > 0:
+            action = params[0].upper()
+
+            if action == "LIST":
+                cont.chat(send_to, sorted(self.library.sections()))
+
+            elif action == "ADD":
+                if len(params) > 2:
+                    old_section = params[1].upper()
+                    url = params[2]
+
+                    if "://" not in old_section:
+                        try:
+                            self.library.add_section(old_section)
+                            if self.library.has_section(old_section):  # if added correctly
+                                self.library.set(old_section, "url", url)
+                                cont.chat(send_to, "{0} was added.".format(old_section))
+                        except DuplicateSectionError:  # if already exists
+                            cont.chat(send_to, "Book already exists. Use SET to edit existing entry.")
+                        except ValueError:  # if default name
+                            cont.chat(send_to, "Can't have name of 'DEFAULT'")
+                    else:
+                        cont.chat(send_to, "Name can't be a URL.")
+                else:
+                    cont.chat(send_to, "Usage: .lib add NAME URL")
+
+            elif action == "DEL":
+                if len(params) > 1:
+                    old_section = params[1].upper()
+
+                    if self.library.remove_section(old_section):
+                        cont.chat(send_to, "{0} was removed.".format(old_section))
+                    else:
+                        cont.chat(send_to, "No such book.")
+                else:
+                    cont.chat(send_to, "Usage: .lib del NAME")
+
+            elif action == "GET":
+                if len(params) > 1:
+                    old_section = params[1].upper()
+
+                    if self.library.has_section(old_section):
+                        cont.chat(send_to,
+                                  "{0} - {1}".format(old_section, self.library.get(old_section, "url", raw=True)))
+                    else:
+                        cont.chat(send_to, "No such book.")
+                else:
+                    cont.chat(send_to, "Usage: .lib get NAME")
+
+            elif action == "SET":
+                if len(params) > 3:
+                    old_section = params[1].upper()
+                    new_section = params[2].upper()
+                    url = params[3]
+
+                    if "://" not in old_section and "://" not in new_section:
+                        if self.library.has_section(old_section):
+                            if old_section != new_section:
+                                self.library.remove_section(old_section)
+                                self.library.add_section(new_section)
+
+                            self.library.set(new_section, "url", url)
+                            cont.chat(send_to, "{0} was edited.".format(old_section))
+                        else:
+                            cont.chat(send_to, "No such book. Use ADD to create a new entry.")
+                    else:
+                        cont.chat(send_to, "Name can't be a URL.")
+                else:
+                    cont.chat(send_to, "Usage: .lib set OLD_NAME NEW_NAME URL")
+
+            else:
+                cont.chat(send_to, "Invalid subcommand.")
+
+        else:
+            cont.chat(send_to, "Please specify a subcommand: list, add, del, get, set")
+
+        # Save changes
+        with open("data/library.ini", "w") as configfile:
+            self.library.write(configfile)
